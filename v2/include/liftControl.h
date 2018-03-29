abe3f9253f62c2ddf9b1f5e2042aabf5131abcec
zdog1230 <zdog1230@yahoo.com>
2018-03-29 18:27:08 -0400

    Setup V2 Robot

---
+++ v2/include/liftControl.h
@@ -0,0 +1,13 @@
+/**
+ * Functions for controlling the lift
+ */
+
+#include "main.h"
+
+#define LIFT_DOWN 1400
+#define LIFT_HALF 1300
+#define LIFT_UP 9
+
+void liftControl(void *parameter);
+void setLiftAngle(int liftAngle);
+
